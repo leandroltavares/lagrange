159800aa2847da76dbd87b60773845ee5473e927
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2018-09-04 22:04:10 -0300

    initial lagrange implementation
    initial lagrange implementation

---
+++ lagrange.js
@@ -0,0 +1,54 @@
+var Lagrange = function(x1, y1, x2, y2) {
+
+	this.xs = [x1, x2];
+	this.ys = [y1, y2];
+	this.ws = [];
+}
+
+Lagrange.prototype.addPoint = function(x, y) {
+	this.xs.push(x);
+	this.ys.push(y);
+	return this.pointCount() - 1;
+}
+
+Lagrange.prototype.removePoint = function (index) {
+	this.xs.remove(index);
+	this.ys.remove(index);
+	return this.pointCount();
+
+};
+
+Lagrange.prototype.pointCount = function () {
+	return this.xs.length;
+};
+
+Lagrange.prototype.generateLexpression = function (index) {
+	var numerator = "", denominator = "";
+
+	for (var j = 0; j < this.pointCount(); j++) {
+		 if(index != j){
+			 numerator += `(x - ${this.xs[j]}) *`;
+			 denominator += `(${this.xs[index]} - ${this.xs[j]}) *`;
+		 }
+	}
+
+	return `${numerator.substr(0, numerator.length - 1)}/ ${denominator.substr(0, denominator.length - 1)}`;
+};
+
+Lagrange.prototype.generatePexpression = function () {
+	var expression = "";
+	for (var i = 0; i < this.pointCount(); i++) {
+		expression += `(${this.generateLexpression(i)} * ${(this.ys[i])} ) +`;
+	}
+	return expression.substr(0, expression.length - 1);
+};
+
+Lagrange.prototype.evaluateExpression = function(x){
+	var compiled = math.compile(this.generatePexpression());
+	var scope = {x: x};
+	return compiled.eval(scope);
+}
+
+Lagrange.prototype.eval = function (x) {
+	//TODO
+};
